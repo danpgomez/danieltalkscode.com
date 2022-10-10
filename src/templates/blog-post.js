e92e8ebf36fa9dbb70d1cd8d389da35f2ebcebee
danpgomez <dapgo3@gmail.com>
2022-10-10 16:45:04 -0400

    Remove disqus import

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import { Disqus, CommentCount } from "gatsby-plugin-disqus"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
